c250328d26b09b86344046f82898bd31c979ba2e
chin <a5566qq2581@gmail.com>
2021-09-03 21:18:29 +0800

    feat: 新增用學期成績檢查登入狀態的API

--- routes/api/stuInfo/index.js
+++ routes/api/stuInfo/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get('/' , require('./controller/get_StuInfo'))
 router.post('/' , require('./controller/post_StuInfo'));
+router.get('/loginStatus', require('./controller/get_loginStatus'));
 
 module.exports = router;
