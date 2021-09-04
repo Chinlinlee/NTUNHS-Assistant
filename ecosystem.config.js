6041a9efcd8fa6834d3d4a1411ecbff7c3d7a4e1
chin <a5566qq2581@gmail.com>
2021-09-04 18:24:02 +0800

    feat: add update Course schedule
    chore: rename file name
    
    build: pm2 add traffic, course update and check sign off app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,57 @@
 module.exports = {
-    apps: [{
-        name: 'mycosim',
-        script: 'server.js',
-        watch: false,
-        ignore_watch: ["node_modules", "pm2log", "docx", "pdf", "picture", "xlsx", "*.log"],
-        exec_mode: 'cluster',
-        instances: 5,
-        max_memory_restart: '1000M',
-        time: true,
-        log_date_format: 'YYYY-MM-DD HH:mm Z',
-        force: true,
-        wait_ready: false,
-        max_restarts: 10,
-        // 單位為 ms, 預設為 0, 若有指定時間，則 app 會等待指定時間過後重啟
-        autorestart: true,
-        error_file: './pm2log/err.log',
-        // 正常輸出 log 的指定位置
-        out_file: './pm2log/out.log',
-        log_file: './pm2log/log.log'
-    }]
+    apps: [
+        {
+            name: 'mycosim',
+            script: 'server.js',
+            watch: false,
+            ignore_watch: ["node_modules", "pm2log", "docx", "pdf", "picture", "xlsx", "*.log"],
+            exec_mode: 'cluster',
+            instances: 5,
+            max_memory_restart: '1000M',
+            time: true,
+            log_date_format: 'YYYY-MM-DD HH:mm Z',
+            force: true,
+            wait_ready: false,
+            max_restarts: 10,
+            // 單位為 ms, 預設為 0, 若有指定時間，則 app 會等待指定時間過後重啟
+            autorestart: true,
+            error_file: './pm2log/err.log',
+            // 正常輸出 log 的指定位置
+            out_file: './pm2log/out.log',
+            log_file: './pm2log/log.log'
+        },
+        {
+            name: "mycosim-traffic",
+            script: "getTraffic.js",
+            watch: false,
+            exec_mode: 'fork',
+            log_date_format: 'YYYY-MM-DD HH:mm Z',
+            error_file: './pm2log/traffic-err.log',
+            // 正常輸出 log 的指定位置
+            out_file: './pm2log/traffic-out.log',
+            log_file: './pm2log/traffic-log.log'
+        },
+        {
+            name: "mycosim-update",
+            script: "models/NTUNHS/updateCourseSchedule.js",
+            watch: false,
+            exec_mode: 'fork',
+            log_date_format: 'YYYY-MM-DD HH:mm Z',
+            error_file: './pm2log/course-update-err.log',
+            // 正常輸出 log 的指定位置
+            out_file: './pm2log/course-update-out.log',
+            log_file: './pm2log/course-update-log.log'
+        },
+        {
+            name: "mycosim-checkSignOff ",
+            script: "models/NTUNHS/checkSignOffLessTakeSchedule.js",
+            watch: false,
+            exec_mode: 'fork',
+            log_date_format: 'YYYY-MM-DD HH:mm Z',
+            error_file: './pm2log/course-update-err.log',
+            // 正常輸出 log 的指定位置
+            out_file: './pm2log/course-update-out.log',
+            log_file: './pm2log/course-update-log.log'
+        }
+    ]
 };
---
+++ models/NTUNHS/updateCourseSchedule.js
@@ -0,0 +1,7 @@
+const { updateCourseMain } = require('./updateCourse');
+const schedule = require('node-schedule');
+updateCourseMain();
+let scheduleUpdateCourse = schedule.scheduleJob({rule: '0 30 0 * * *'} ,function () {
+  console.log("update course");
+  updateCourseMain();
+});
