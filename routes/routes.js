83ec14c62c1527ff84e63932f963ef2f4ff281fc
chin <a5566qq2581@gmail.com>
2022-06-23 21:36:07 +0800

    feat: 新增免責聲明頁面

--- routes/routes.js
+++ routes/routes.js
@@ -248,6 +248,15 @@ module.exports =
                 IsUpdate: true,
             });
         });
+
+        app.get('/privacy-policy', function (req, res) {
+            res.header(
+                'Cache-Control',
+                'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'
+            );
+            // res.locals.CurrentUser = req.user;
+            res.render('./atm/privacy-policy.html');
+        });
         app.route('/api/log', My_Func.IsLoggedIn).all(async function (
             req,
             res,
