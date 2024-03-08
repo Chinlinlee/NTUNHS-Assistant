a7f080464e4824bf3e01bd3b7d0ce76364414de0
CHIN\a5566 <a5566qq2581@gmail.com>
2024-03-08 14:28:15 +0800

    test: 必須使用 config 的帳號密碼

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,11 +1,12 @@
 const { describe, it } = require("node:test");
 const assert = require("node:assert");
 const { NtunhsAuthHandler } = require("../models/users/School_Auth");
+const ntunhsConfig = require("../models/NTUNHS/config");
 
 describe("portfolio login", () => {
     it("should login successful", async () => {
         let ntunhsAuthHandler = new NtunhsAuthHandler({ session: {} });
-        let authResult = await ntunhsAuthHandler.portfolioAuth("062214211", "a9TA4oNS");
+        let authResult = await ntunhsAuthHandler.portfolioAuth(ntunhsConfig.stuNum, ntunhsConfig.stuPwd);
         console.log(authResult);
     })
 });
